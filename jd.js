fc33cc2025ec9c925de789a2ba091cf4de17bfc9
kmrule <128225365+kmrule@users.noreply.github.com>
2023-04-30 02:06:13 +0800

    Update jd.js

--- jd.js
+++ jd.js
@@ -1,41 +1,28 @@
-// 获取当前节点信息
-var nodeInfo = $surge.getNodeInfo();
-var nodeName = nodeInfo.nodeName;
-var nodeIP = nodeInfo.ip;
-
-// 构造请求头
-var headers = {
-  'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Mobile/15E148 Safari/604.1',
-  'Host': 'policies.google.com',
-  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-  'Accept-Language': 'zh-cn',
-  'Connection': 'close'
+// 添加自定义请求头
+var options = {
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1'
+  },
+  timeout: 5000 // 设置超时时间为 5 秒
 };
 
-// 构造请求
-var url = 'https://policies.google.com/terms?hl=zh-CN';
-var method = 'GET';
-var body = '';
-var request = {
-  url: url,
-  method: method,
-  headers: headers,
-  body: body
-};
-
-// 发送请求
-$task.fetch(request).then(response => {
-  // 解析文本内容
-  var text = $response.body.text();
-  var regex = /中国/;
-  var isBlocked = regex.test(text);
-
-  // 输出结果
-  if (isBlocked) {
-    console.log(nodeName + '(' + nodeIP + ')' + ' 已被送中');
-  } else {
-    console.log(nodeName + '(' + nodeIP + ')' + ' 未被送中');
+// 发送 HTTPS 请求
+$httpClient.get('https://policies.google.com/terms?hl=zh-CN', options, function(error, response, data) {
+  if (error) {
+    $notification.post('请求出错', error, '');
+    return;
   }
-}, reason => {
-  console.log(nodeName + '(' + nodeIP + ')' + ' 请求失败');
+
+  // 解析 HTML 文档
+  var doc = $($.parseHTML(data));
+  
+  // 获取指定路径下的文本内容
+  var text = doc.find('#yDmH0d c-wiz div:nth-child(2) div:nth-child(4) div:nth-child(4) c-wiz div:nth-child(1) div:nth-child(2) p:nth-child(2)').text();
+
+  // 判断文本内容是否包含 "中国"
+  if (text.includes('中国')) {
+    $notification.post('结果', '是中国：' + text, '');
+  } else {
+    $notification.post('结果', '不是中国：' + text, '');
+  }
 });
