064972ccbf12df7e0859ff174e1a4fb51f7a4d7e
kmrule <128225365+kmrule@users.noreply.github.com>
2023-04-30 01:41:27 +0800

    Update jd.js

--- jd.js
+++ jd.js
@@ -1,9 +1,32 @@
-$task.fetch("https://policies.google.com/terms?hl=zh-CN").then(response => {
-    if (response.statusCode == 200) {
-        console.log(response.body);
+// ==圈X配置==
+// [General]
+// 标题=测试脚本
+// 程序=js
+// 版本=1.0
+// 长期缓存=true
+
+// [Script]
+// http-request https://policies.google.com/terms?hl=zh-CN script-path=https://raw.githubusercontent.com/ConnersHua/Profiles/master/Surge/Scripting/gp.js,requires-body=true
+// ==圈X配置==
+
+// 前置准备
+var url = $request.url;
+var method = $request.method;
+var headers = $request.headers;
+var body = $request.body;
+
+// 请求页面
+$http.request({
+    method: method,
+    url: url,
+    headers: headers,
+    body: body
+}, function(error, response, data){
+    if (error) {
+        console.log(error);
+        $done();
     } else {
-        console.log(`请求失败，状态码：${response.statusCode}`);
+        console.log(data);
+        $done();
     }
-}, reason => {
-    console.log(`请求异常：${reason.error}`);
 });
