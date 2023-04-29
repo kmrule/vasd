300eea5732db6fe258498c8ab06c351752d9e6bf
kmrule <128225365+kmrule@users.noreply.github.com>
2023-04-30 02:09:11 +0800

    Update jd.js

--- jd.js
+++ jd.js
@@ -1,21 +1,21 @@
-// 添加自定义请求头
-var options = {
-  headers: {
-    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1'
-  },
-  timeout: 5000 // 设置超时时间为 5 秒
+// 定义请求头
+const headers = {
+  'Accept-Encoding': 'gzip, deflate, br',
+  'Connection': 'keep-alive',
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+  'Host': 'www.google.com',
+  'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Mobile/15E148 Safari/604.1',
+  'Accept-Language': 'zh-CN,zh-Hans;q=0.9'
 };
 
-// 发送 HTTPS 请求
-$httpClient.get('https://policies.google.com/terms?hl=zh-CN', options, function(error, response, data) {
-  if (error) {
-    $notification.post('请求出错', error, '');
-    return;
-  }
-
+// 发送 HTTP 请求
+$task.fetch({
+  url: 'https://policies.google.com/terms?hl=zh-CN',
+  headers: headers
+}).then(response => {
   // 解析 HTML 文档
-  var doc = $($.parseHTML(data));
-  
+  var doc = $($.parseHTML(response.body));
+
   // 获取指定路径下的文本内容
   var text = doc.find('#yDmH0d c-wiz div:nth-child(2) div:nth-child(4) div:nth-child(4) c-wiz div:nth-child(1) div:nth-child(2) p:nth-child(2)').text();
 
@@ -25,4 +25,7 @@ $httpClient.get('https://policies.google.com/terms?hl=zh-CN', options, function(
   } else {
     $notification.post('结果', '不是中国：' + text, '');
   }
+}, reason => {
+  // 处理请求错误
+  $notification.post('请求出错', reason.error, '');
 });
