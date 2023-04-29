cd92c9ae9df042dfc1ed55fa7d24b417f0a91dc8
kmrule <128225365+kmrule@users.noreply.github.com>
2023-04-30 01:31:01 +0800

    Update jd.js

--- jd.js
+++ jd.js
@@ -1,24 +1,8 @@
-// ==[Script File]==
-// @name            Google Policies Page Content
-// @description     Request the Google policies page and output the content
-// @icon            url-to-icon
-// @category        Utility
-// @version         1
-// ==[End Script]==
-
-// Request the Google policies page
-$http.get({
-  url: 'https://policies.google.com/terms?hl=zh-CN',
-  headers: {
-    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Mobile/15E148 Safari/604.1'
-  }
-}, (error, response, data) => {
-  if (error) {
-    console.log('Error:', error);
+$task.fetch("https://policies.google.com/terms?hl=zh-CN").then(response => {
+    console.log(response.body);
+}, reason => {
+    console.log(reason.error);
     $done();
-  } else {
-    // Output the content of the page
-    console.log(data);
-    $done();
-  }
 });
+
+    
