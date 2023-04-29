48b9d0116daddf693b1a0f12f701b2dbab040f15
kmrule <128225365+kmrule@users.noreply.github.com>
2023-04-29 20:23:52 +0800

    Update jd.js

--- jd.js
+++ jd.js
@@ -1,9 +1,18 @@
-$task.fetch("https://policies.google.com/terms?hl=zh-CN").then(response => {
-    if (response.body.indexOf("中国") !== -1) {
-        console.log("当前网络被送中");
+$task.fetch({ 
+  url: "https://policies.google.com/terms?hl=zh-CN", 
+  headers: {
+    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 15_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Mobile/15E148 Safari/604.1",
+  } 
+}).then(response => {
+  if (response.statusCode == 200) {
+    if (response.body.indexOf("中国") != -1) {
+      console.log("当前节点已被送中");
     } else {
-        console.log("当前网络未被送中");
+      console.log("当前节点未被送中");
     }
+  } else {
+    console.log("请求失败，状态码：" + response.statusCode);
+  }
 }, reason => {
-    console.log("请求失败");
+  console.log("请求失败，原因：" + reason.error);
 });
